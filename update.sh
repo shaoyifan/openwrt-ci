2cb90bc1e4899b81623746f20c53bd9793b57a05
shaoyf <shaoyifan@live.com>
2025-06-02 18:48:30 +0800

    remove update_affinity_script

--- update.sh
+++ update.sh
@@ -498,7 +498,7 @@ main() {
     add_wifi_default_set
     update_default_lan_addr
     # remove_something_nss_kmod
-    update_affinity_script
+    # update_affinity_script
     chanage_cpuusage
     add_ax6600_led
     update_nss_pbuf_performance
